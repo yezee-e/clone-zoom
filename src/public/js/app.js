88ab9769dda8dc79d1fa8a324ad4820b3b70f3da
yeji <yejipooh@gmail.com>
2024-10-04 20:17:15 +0900

    feat: raeltime chatting

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,40 +1,55 @@
-const messageList = document.querySelector('ul');
-const nickForm = document.querySelector('#nick');
-const messageForm = document.querySelector('#message');
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io(); //io()는 socket.io를 실행하고 있는 서버를 찾음
 
-function makeMessage(type, payload) {
-    const msg = { type, payload };
-    return JSON.stringify(msg);
-}
+const welcome = document.getElementById('welcome');
+const form = welcome.querySelector('form');
+const room = document.getElementById('room');
 
-socket.addEventListener('open', () => {
-    console.log('Connected to server🍒');
-});
+room.hidden = true;
 
-socket.addEventListener('message', (message) => {
+let roomName;
+
+function addMessage(message) {
+    const ul = room.querySelector('ul');
     const li = document.createElement('li');
-    li.innerText = message.data;
-    messageList.append(li);
-});
+    li.innerText = message;
+    ul.appendChild(li);
+}
 
-socket.addEventListener('close', () => {
-    console.log('Disconnected to server 😡');
-});
-
-function handleSubmit(e) {
+function handleMessageSubmit(e) {
     e.preventDefault();
-    const input = messageForm.querySelector('input');
-    socket.send(makeMessage('new_message', input.value));
+    const input = room.querySelector('input');
+    const value = input.value;
+    socket.emit('new_message', input.value, roomName, () => {
+        addMessage(`You:${value}`);
+    });
     input.value = '';
 }
 
-function handleNickSubmit(e) {
+function showRoom() {
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector('h3');
+    h3.innerText = `room ${roomName}`;
+    const form = room.querySelector('form');
+    form.addEventListener('submit', handleMessageSubmit);
+}
+
+function handleRoomSubmit(e) {
     e.preventDefault();
-    const input = nickForm.querySelector('input');
-    socket.send(makeMessage('nickname', input.value));
+    const input = form.querySelector('input');
+    socket.emit('enter_room', input.value, showRoom);
+    roomName = input.value;
     input.value = '';
 }
 
-messageForm.addEventListener('submit', handleSubmit);
-nickForm.addEventListener('submit', handleNickSubmit);
+form.addEventListener('submit', handleRoomSubmit);
+
+socket.on('welcome', () => {
+    addMessage('someone joined!!');
+});
+
+socket.on('bye', () => {
+    addMessage('someone left ㅜㅜ');
+});
+
+socket.on('new_message', addMessage);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from 'http';
 import express from 'express';
-import { WebSocketServer } from 'ws';
-// import SocketIO from 'socket.io';
+// import { WebSocketServer } from 'ws';
+import SocketIO from 'socket.io';
 
 const app = express();
 
@@ -11,40 +11,66 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
-const handleListen = () => console.log('Listening on http://localhost:3005');
-
 ////////////////////////////////websocket///////////////////////////////////////////////////
 
-const server = http.createServer(app);
-const wss = new WebSocketServer({ server }); //http서버와 ws서버가 함께 존재
-const sockets = [];
+// const handleListen = () => console.log('Listening on http://localhost:3005');
+// const server = http.createServer(app);
+// const wss = new WebSocketServer({ server }); //http서버와 ws서버가 함께 존재
+// const sockets = [];
 
-wss.on('connection', (socket) => {
-    sockets.push(socket);
-    socket['nickname'] = 'Anon';
-    // console.log(socket);
-    console.log('connected to Browser🥳');
-    socket.on('close', () => {
-        console.log('Disconnected from server🥵');
-    });
-    socket.on('message', (message) => {
-        const parsed = JSON.parse(message);
-        switch (parsed.type) {
-            case 'new_message':
-                sockets.forEach((aSocket) =>
-                    aSocket.send(`${socket.nickname}:${parsed.payload}`)
-                );
-                break;
-            case 'nickname':
-                socket['nickname'] = parsed.payload;
-        }
+// wss.on('connection', (socket) => {
+//     sockets.push(socket);
+//     socket['nickname'] = 'Anon';
+//     // console.log(socket);
+//     console.log('connected to Browser🥳');
+//     socket.on('close', () => {
+//         console.log('Disconnected from server🥵');
+//     });
+//     socket.on('message', (message) => {
+//         const parsed = JSON.parse(message);
+//         switch (parsed.type) {
+//             case 'new_message':
+//                 sockets.forEach((aSocket) =>
+//                     aSocket.send(`${socket.nickname}:${parsed.payload}`)
+//                 );
+//                 break;
+//             case 'nickname':
+//                 socket['nickname'] = parsed.payload;
+//         }
 
-        // socket.send(message.toString('utf-8'));
-    });
-});
-server.listen(3005, handleListen);
+//          socket.send(message.toString('utf-8'));
+//     });
+// });
+// server.listen(3005, handleListen);
 
 //////////////////////////////////////////socket.io///////////////////////////////////////////
+const handleListen = () => console.log('Listening on http://localhost:3005');
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
+httpServer.listen(3005, handleListen);
 
-// const wsServer=SocketIO(httpServer);
-// httpServer.listen(3005, handleListen);
+wsServer.on('connection', (socket) => {
+    socket.onAny((e) => {
+        console.log(`Socket event ${e}`);
+    });
+
+    socket.on('enter_room', (roomName, done) => {
+        socket.join(roomName); //방에입장
+        done(); //방에입장을 프론트에 알림
+        socket.to(roomName).emit('welcome'); //본인을 제외하고 방안모두에게 입장알림
+        socket.on('disconnecting', () => {
+            socket.rooms.forEach((room) => {
+                socket.to(room).emit('bye');
+            });
+        }); // 브라우저는 이미 닫았지만 아직 연결이 끊어지지 않은 그 찰나에 발생하는 이벤트
+        socket.on('new_message', (msg, room, done) => {
+            socket.to(room).emit('new_message', msg);
+            done();
+        });
+    });
+});
+
+/*disconnect : 연결이 완전히 끊어졌을때 발생하는 이벤트 (room 정보가 비어있음)
+disconnecting : 브라우져는 이미 닫았지만 아직 연결이 끊어지지 않은 그 찰나에
+발생하는 이벤트 (그래서 room 정보가 살아있음)
+*/
